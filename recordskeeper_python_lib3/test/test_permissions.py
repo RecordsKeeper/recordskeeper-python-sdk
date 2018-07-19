851651905aac4aa4ef030014c8f1ac2ed571baf5
trinayanbhatt <trinayanbhatt1@gmail.com>
2018-07-19 13:25:01 +0530

    check added

--- recordskeeper_python_lib3/test/test_permissions.py
+++ recordskeeper_python_lib3/test/test_permissions.py
@@ -1,58 +1,58 @@
-import unittest
-import yaml
-import binascii
-from recordskeeper_python_lib3 import permissions
-from recordskeeper_python_lib3.permissions import Permissions
+# import unittest
+# import yaml
+# import binascii
+# from recordskeeper_python_lib3 import permissions
+# from recordskeeper_python_lib3.permissions import Permissions
 
-import sys
+# import sys
 
-import os.path
+# import os.path
 
-if (os.path.exists("config.yaml")):
-   with open("config.yaml", 'r') as ymlfile:
-      cfg = yaml.load(ymlfile)
+# if (os.path.exists("config.yaml")):
+#    with open("config.yaml", 'r') as ymlfile:
+#       cfg = yaml.load(ymlfile)
       
-      network = cfg['network']
+#       network = cfg['network']
 
-      url = network['url']
-      user = network['rkuser']
-      password = network['passwd']
-      chain = network['chain']
-      net = address.network
-else:
+#       url = network['url']
+#       user = network['rkuser']
+#       password = network['passwd']
+#       chain = network['chain']
+#       net = address.network
+# else:
    
-   url = os.environ['url']
-   user = os.environ['rkuser']
-   password = os.environ['passwd']
-   chain = os.environ['chain']
-   net = os.environ 
+#    url = os.environ['url']
+#    user = os.environ['rkuser']
+#    password = os.environ['passwd']
+#    chain = os.environ['chain']
+#    net = os.environ 
 
-class PermissionsTest(unittest.TestCase):
+# class PermissionsTest(unittest.TestCase):
 
 
-    def test_grantpermissions(self):
+#     def test_grantpermissions(self):
         
-        txid = Permissions.grantPermission(self, net['permissionaddress'], "create, connect")
-        self.assertEqual(txid, 'Invalid permission')
+#         txid = Permissions.grantPermission(self, net['permissionaddress'], "create, connect")
+#         self.assertEqual(txid, 'Invalid permission')
 
-    def test_revokepermissions(self):
+#     def test_revokepermissions(self):
 
-        txid = Permissions.revokePermission(self, net['permissionaddress'], "send, admin")
-        self.assertEqual(txid, 'Invalid permission')
-
-
-    def test_failgrantpermissions(self):
-
-    	txid = Permissions.grantPermission(self, net['permissionaddress'], "create, connect")
-    	self.assertEqual(txid, 'e3bba87d1f0a980b65f12388d31c734ea38b08d11d00aaab1004e470ca419556')
+#         txid = Permissions.revokePermission(self, net['permissionaddress'], "send, admin")
+#         self.assertEqual(txid, 'Invalid permission')
 
 
-    def test_failrevokepermissions(self):
+#     def test_failgrantpermissions(self):
 
-    	txid = Permissions.revokePermission(self, net['permissionaddress'], "create, connect")
-    	self.assertEqual(txid, 'e3bba87d1f0a980b65f12388d31c734ea38b08d11d00aaab1004e470ca419556')
+#     	txid = Permissions.grantPermission(self, net['permissionaddress'], "create, connect")
+#     	self.assertEqual(txid, 'e3bba87d1f0a980b65f12388d31c734ea38b08d11d00aaab1004e470ca419556')
+
+
+#     def test_failrevokepermissions(self):
+
+#     	txid = Permissions.revokePermission(self, net['permissionaddress'], "create, connect")
+#     	self.assertEqual(txid, 'e3bba87d1f0a980b65f12388d31c734ea38b08d11d00aaab1004e470ca419556')
         
 
-if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(PermissionsTest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+# if __name__ == '__main__':
+#     suite = unittest.TestLoader().loadTestsFromTestCase(PermissionsTest)
+#     unittest.TextTestRunner(verbosity=2).run(suite)
